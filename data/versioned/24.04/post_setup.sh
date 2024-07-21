f423628093a3bf17a750053e858ea09b5fec11b4
georglauterbach <44545919+georglauterbach@users.noreply.github.com>
2024-07-21 20:11:50 +0200

    rework `post_setup.sh` for Ubuntu 24.04

--- data/versioned/24.04/post_setup.sh
+++ data/versioned/24.04/post_setup.sh
@@ -1,46 +1,62 @@
 #! /usr/bin/env bash
 
-readonly DOAS_CONFIG_FILE='/etc/doas.conf'
-if [[ ! -e ${DOAS_CONFIG_FILE} ]]; then
-  log 'debug' "Setting up 'doas'"
-  echo "permit persist ${USER}" >"${DOAS_CONFIG_FILE}"
-  chown root:root "${DOAS_CONFIG_FILE}"
-  chmod 0400 "${DOAS_CONFIG_FILE}"
+function setup_doas() {
+  local DOAS_CONFIG_FILE='/etc/doas.conf'
+  readonly DOAS_CONFIG_FILE
+  if [[ ! -e ${DOAS_CONFIG_FILE} ]]; then
+    log 'debug' "Setting up 'doas'"
+    echo "permit persist ${USER}" >"${DOAS_CONFIG_FILE}"
+    chown root:root "${DOAS_CONFIG_FILE}"
+    chmod 0400 "${DOAS_CONFIG_FILE}"
 
-  if doas -C "${DOAS_CONFIG_FILE}"; then
-    log 'trace' 'Configuration for doas looks good'
-  else
-    log 'warn' 'doas configuration has errors - do not use it immediately'
+    if doas -C "${DOAS_CONFIG_FILE}"; then
+      log 'trace' 'Configuration for doas looks good'
+    else
+      log 'warn' 'doas configuration has errors - do not use it immediately'
+    fi
   fi
-fi
+}
 
-mkdir -p "${HOME}/.gnupg"
-touch "${HOME}/.gnupg/gpg-agent.conf"
-if grep -v -q 'pinentry-program' "${HOME}/.gnupg/gpg-agent.conf"; then
-  echo 'pinentry-program /usr/bin/pinentry-curses' >>"${HOME}/.gnupg/gpg-agent.conf"
-  gpg-connect-agent reloadagent /bye
-fi
+function setup_gnupg() {
+  local GNUPG_HOME_DIR="${HOME}/.gnupg"
+  mkdir -p "${GNUPG_HOME_DIR}"
+  touch "${GNUPG_HOME_DIR}/gpg-agent.conf"
+  if ! grep -q 'pinentry-program' "${GNUPG_HOME_DIR}/gpg-agent.conf"; then
+    echo 'pinentry-program /usr/bin/pinentry-curses' >>"${GNUPG_HOME_DIR}/gpg-agent.conf"
+    gpg-connect-agent reloadagent /bye || :
+  fi
+  chown -R "${USER}:${USER}" "${GNUPG_HOME_DIR}"
+}
 
-# shellcheck disable=SC2154
-if [[ ${GUI} -eq 1 ]]; then
-  log 'debug' 'To change the bookmarks in Nautilus, edit ~/.config/user-firs.dirs, ~/.config/gtk-3.0/bookmarks, and /etc/xdg/user-dirs.defaults'
-fi
+function setup_snap() {
+  if command -v snap &>/dev/null; then
+    log 'info' "Purging 'snapd'"
 
-if command -v snap &>/dev/null; then
-  log 'info' "Purging 'snapd'"
+    killall snap
+    systemctl stop snapd
 
-  killall snap
-  systemctl stop snapd
+    until [[ $(snap list 2>&1 || :) == 'No snaps'*'installed'* ]]; do
+      while read -r SNAP _; do
+        snap remove --purge "${SNAP}" &>/dev/null || :
+      done < <(snap list |& tail -n +2 || :)
+    done
 
-  until [[ $(snap list 2>&1 || :) == 'No snaps'*'installed'* ]]; do
-    while read -r SNAP _; do
-      snap remove --purge "${SNAP}" &>/dev/null || :
-    done < <(snap list |& tail -n +2 || :)
-  done
+    apt-get -qq purge snapd gnome-software-plugin-snap
+    apt-mark -qq hold snapd
+    rm -rf /var/cache/snapd/ "${HOME}/snapd" "${HOME}/snap"
 
-  apt-get -qq purge snapd gnome-software-plugin-snap
-  apt-mark -qq hold snapd
-  rm -rf /var/cache/snapd/ "${HOME}/snapd" "${HOME}/snap"
+    log 'debug' "Finished purging 'snapd'"
+  fi
+}
 
-  log 'debug' "Finished purging 'snapd'"
-fi
+function setup_misc() {
+  # shellcheck disable=SC2154
+  if [[ ${GUI} -eq 1 ]]; then
+    log 'debug' 'To change the bookmarks in Nautilus, edit ~/.config/user-firs.dirs, ~/.config/gtk-3.0/bookmarks, and /etc/xdg/user-dirs.defaults'
+  fi
+}
+
+setup_doas
+setup_gnupg
+setup_snap
+setup_misc
