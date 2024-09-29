b3017caaa5fbe1e683fd58e353601dc8ebf070ab
georglauterbach <44545919+georglauterbach@users.noreply.github.com>
2024-09-29 16:55:49 +0200

    add script to install Flatpak

---
+++ misc/flatpak.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+SUDO_CMD="add-apt-repository --yes --no-update ppa:flatpak/stable"
+SUDO_CMD+=" && apt-get --yes update"
+SUDO_CMD+=" && apt-get --yes install flatpak malcontent-gui steam-devices"
+
+sudo bash -c "${SUDO_CMD}"
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+echo "Restart you machine for changes to take effect"
+
