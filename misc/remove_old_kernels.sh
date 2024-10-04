35abe69e29d63a639a7ba3a6790404849dd3bf70
Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>
2024-10-04 15:47:49 +0200

    add script to remove old kernel versions

---
+++ misc/remove_old_kernels.sh
@@ -0,0 +1,31 @@
+#! /usr/bin/env bash
+
+if [[ ${EUID} -ne 0 ]]; then
+  echo "This script needs to run with superuser privileges" >&2
+  exit 1
+fi
+
+function purge_old_config_files() {
+  local UNNECESSARY_CONFIG_FILES
+  readarray -t UNNECESSARY_CONFIG_FILES < <(dpkg -l | awk '/^rc/{print $2}')
+  if [[ ${#UNNECESSARY_CONFIG_FILES} -gt 0 ]]; then
+    dpkg --purge "${UNNECESSARY_CONFIG_FILES[@]}"
+  fi
+}
+
+function purge_old_kernels() {
+  local CURRENT_KERNEL_VERSION OLD_LINUX_IMAGES OLD_LINUX_HEADERS
+
+  CURRENT_KERNEL_VERSION=$(uname -r | sed -E "s/([0-9.-]*)-([^0-9]+)/\1/")
+  readarray -t OLD_LINUX_IMAGES  < <(dpkg --list | grep 'linux-image'   | awk '{print $2}' \
+    | sort -V | sed -n "/$(uname -r)/q;p")
+  readarray -t OLD_LINUX_HEADERS < <(dpkg --list | grep 'linux-headers' | awk '{print $2}' \
+    | sort -V | sed -n "/${CURRENT_KERNEL_VERSION}/q;p")
+
+  apt-get --yes purge "${OLD_LINUX_IMAGES[@]}" "${OLD_LINUX_HEADERS[@]}"
+  apt-get --yes autoremove --purge
+}
+
+purge_old_config_files
+purge_old_kernels
+purge_old_config_files
