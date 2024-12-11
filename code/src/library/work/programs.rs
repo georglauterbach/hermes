d91b5a5f6b1a51721415d8e4485603e3bb06bbaf
Georg Lauterbach <georg.lauterbach@rohde-schwarz.com>
2024-12-11 10:29:07 +0000

    chore: improve wording in doc string

--- code/src/library/work/programs.rs
+++ code/src/library/work/programs.rs
@@ -9,14 +9,16 @@ use ::async_std::stream::StreamExt as _;
 /// The architecture string for the amd64 (`x86_64`) architecture
 #[cfg(target_arch = "x86_64")]
 const ARCHITECTURE: &str = "x86_64";
-/// The library to link against.
+/// The library that is linked against by programs. Not all programs
+/// support `musl`, especially on `aarch64`.
 #[cfg(target_arch = "x86_64")]
 const LINK_LIBRARY: &str = "musl";
 
 /// The architecture string for the arm64 (`aarch64`) architecture
 #[cfg(target_arch = "aarch64")]
 const ARCHITECTURE: &str = "aarch64";
-/// The library to link against.
+/// The library that is linked against by programs. Not all programs
+/// support `musl`, especially on `aarch64`.
 #[cfg(target_arch = "aarch64")]
 const LINK_LIBRARY: &str = "gnu";
 
