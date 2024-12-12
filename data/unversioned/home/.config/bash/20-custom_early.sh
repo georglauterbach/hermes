f56809a6ab9939904570fe468afefebc568845e1
georglauterbach <44545919+georglauterbach@users.noreply.github.com>
2024-12-12 09:45:53 +0000

    chore: use variables if already set for hermes setup

--- data/unversioned/home/.config/bash/20-custom_early.sh
+++ data/unversioned/home/.config/bash/20-custom_early.sh
@@ -12,12 +12,12 @@
 
 # The variables control whether additional programs ought to be initialized.
 
-export HERMES_INIT_ATUIN=false
-export HERMES_INIT_BAT=false
-export HERMES_INIT_BLE_SH=false
-export HERMES_INIT_FZF=false
-export HERMES_INIT_STARSHIP=false
-export HERMES_INIT_ZOXIDE=false
+export HERMES_INIT_ATUIN=${HERMES_INIT_ATUIN:-false}
+export HERMES_INIT_BAT=${HERMES_INIT_BAT:-false}
+export HERMES_INIT_BLE_SH=${HERMES_INIT_BLE_SH:-false}
+export HERMES_INIT_FZF=${HERMES_INIT_FZF:-false}
+export HERMES_INIT_STARSHIP=${HERMES_INIT_STARSHIP:-false}
+export HERMES_INIT_ZOXIDE=${HERMES_INIT_ZOXIDE:-false}
 
 # -----------------------------------------------
 # ----  System Command Overrides  ---------------
@@ -29,23 +29,23 @@ export HERMES_INIT_ZOXIDE=false
 # Initialization of additonal programs previously is independent
 # of overriding system commands.
 
-export HERMES_OVERRIDE_APT_WITH_NALA=false
-export HERMES_OVERRIDE_CAT_WITH_BAT=false
-export HERMES_OVERRIDE_CD_WITH_ZOXIDE=false
-export HERMES_OVERRIDE_FIND_WITH_FD=false
-export HERMES_OVERRIDE_GREP_WITH_RIPGREP=false
-export HERMES_OVERRIDE_LS_WITH_EZA=false
+export HERMES_OVERRIDE_APT_WITH_NALA=${HERMES_OVERRIDE_APT_WITH_NALA:-false}
+export HERMES_OVERRIDE_CAT_WITH_BAT=${HERMES_OVERRIDE_CAT_WITH_BAT:-false}
+export HERMES_OVERRIDE_CD_WITH_ZOXIDE=${HERMES_OVERRIDE_CD_WITH_ZOXIDE:-false}
+export HERMES_OVERRIDE_FIND_WITH_FD=${HERMES_OVERRIDE_FIND_WITH_FD:-false}
+export HERMES_OVERRIDE_GREP_WITH_RIPGREP=${HERMES_OVERRIDE_GREP_WITH_RIPGREP:-false}
+export HERMES_OVERRIDE_LS_WITH_EZA=${HERMES_OVERRIDE_LS_WITH_EZA:-false}
 
 # -----------------------------------------------
 # ----  Miscellaneous  --------------------------
 # -----------------------------------------------
 
 # This settings loads useful aliases that are very likely to not override anything.
-export HERMES_LOAD_GLOBAL_ALIASES=true
+export HERMES_LOAD_GLOBAL_ALIASES=${HERMES_LOAD_GLOBAL_ALIASES:-false}
 
 # -----------------------------------------------
 # ----  Individual program Configurations  ------
 # -----------------------------------------------
 
 # Configures the location of Atuin's SQLite database file
-export HERMES_CONFIG_ATUIN_DB_FILE=
+export HERMES_CONFIG_ATUIN_DB_FILE=${HERMES_CONFIG_ATUIN_DB_FILE:-false}
