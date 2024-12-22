ca1418fcca86d454ee573d49f5b737ea17cb1665
georglauterbach <44545919+georglauterbach@users.noreply.github.com>
2024-12-22 13:57:02 +0000

    feat: add scripts for font installation
    Signed-off-by: georglauterbach <44545919+georglauterbach@users.noreply.github.com>

---
+++ misc/setup_fonts.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/env bash
+
+set -eE -u -o pipefail
+shopt -s inherit_errexit
+
+if [[ ${EUID} -ne 0 ]]; then
+  echo "ERROR: Run this script with superuser privileges" >&2
+  exit 1
+fi
+
+readonly NERD_FONT_VERSION='v3.3.0'
+
+function download_extract_place() {
+  local FONT_NAME=${1:?Font name required}
+
+  local URI="https://github.com/ryanoasis/nerd-fonts/releases/download/${NERD_FONT_VERSION}/${FONT_NAME}.tar.xz"
+  local TARGET_DIR="/usr/local/share/fonts/${FONT_NAME}-Nerd-Font"
+
+  rm -rf "${TARGET_DIR}"
+  mkdir -p "${TARGET_DIR}"
+  curl -sSfL "${URI}" | tar xJ -C "${TARGET_DIR}"
+  chown -R root:root "${TARGET_DIR}"
+}
+
+download_extract_place 'FiraCode'
+download_extract_place 'JetBrainsMono'
