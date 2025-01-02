1c3be26855bbdd26cd20843373fa0d09e2f702bc
Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>
2025-01-03 00:22:51 +0100

    chore: move and update custom late config example for Bash

---
+++ misc/examples/bash/99-custom_late.sh
@@ -0,0 +1,63 @@
+#! /usr/bin/env bash
+
+# version       0.2.0
+# sourced by    ${HOME}/.bashrc
+# task          user-customizable shell setup
+
+# ! You can edit this file to change the behavior of your shell
+# ! after _hermes_ finished loading.
+
+# shellcheck disable=SC2139
+alias v="${EDITOR:-vi}"
+# shellcheck disable=SC2139
+alias sv="sudo ${EDITOR:-vi}"
+
+if __is_command 'doas'; then
+  complete -cf doas
+  alias sudo='doas'
+fi
+
+if __is_command 'kubectl'; then
+  complete -o default -F __start_kubectl k
+  alias k='kubectl'
+fi
+
+if __is_command 'gitui'; then
+  alias g='gitui'
+elif __is_command 'lazygit'; then
+  alias g='lazygit'
+else
+  alias g='git diff'
+fi
+
+alias gcs='git commit --signoff --gpg-sign'
+function git() {
+  case "${1:-}" in
+    ( 'update' )
+      git fetch -apt
+      git pull
+      ;;
+    ( * )
+      command git "${@}"
+      ;;
+  esac
+}
+
+function theme() {
+  case "${1:-dark}" in
+    ( 'dark' )
+      gsettings set org.gnome.desktop.interface gtk-theme Adwaita-dark
+      gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+      ;;
+
+    ( 'light' )
+      gsettings set org.gnome.desktop.interface gtk-theme Adwaita
+      gsettings set org.gnome.desktop.interface color-scheme prefer-light
+      ;;
+
+    ( * )
+      echo "'${1}' is not a valid option - use 'light' or 'dark'" >&2
+      return 1
+      ;;
+  esac
+}
