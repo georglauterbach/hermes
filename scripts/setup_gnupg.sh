c9cb0272cade99e69641afe2331cb78092a0cd26
Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>
2025-01-13 15:34:11 +0100

    feat: improve GnuPG setup script
    Signed-off-by: Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>

--- scripts/setup_gnupg.sh
+++ scripts/setup_gnupg.sh
@@ -1,9 +1,10 @@
 #! /usr/bin/env -S bash -eE -u -o pipefail -O inherit_errexit
 
-sudo apt-get --yes install gnupg2
-
 readonly GNUPG_HOME_DIR="${HOME}/.gnupg"
 mkdir -p "${GNUPG_HOME_DIR}"
-sudo chown -R "${USER}:${USER}" "${GNUPG_HOME_DIR}"
+
+sudo chown -R "$(id --user):$(id --group)" "${GNUPG_HOME_DIR}"
+sudo apt-get --yes install gnupg2
+
 find "${GNUPG_HOME_DIR}" -type f -exec chmod 600 {} \;
 find "${GNUPG_HOME_DIR}" -type d -exec chmod 700 {} \;
