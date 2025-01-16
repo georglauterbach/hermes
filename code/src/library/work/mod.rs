c9a21bbfd7980eb0f548200bab1012a74e5dc87c
Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>
2025-01-16 14:43:26 +0100

    chore: praise be linters
    Signed-off-by: Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>

--- code/src/library/work/mod.rs
+++ code/src/library/work/mod.rs
@@ -12,8 +12,8 @@ mod update;
 
 /// The base part of URI that we fetch file from.
 const GITHUB_RAW_URI: &str = ::const_format::concatcp!(
-  "https://raw.githubusercontent.com/georglauterbach/hermes/refs/tags/v",
-  env!("CARGO_PKG_VERSION")
+    "https://raw.githubusercontent.com/georglauterbach/hermes/refs/tags/v",
+    env!("CARGO_PKG_VERSION")
 );
 
 /// Does the actual work that _hermes_ is supposed to do.
