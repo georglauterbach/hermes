d6c4f69101843e7318e4ff7af2d055fea92f3c24
Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>
2025-05-18 23:38:48 +0200

    feat: script for installing GUI packages
    Signed-off-by: Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>

---
+++ data/scripts/setup_gui.sh
@@ -0,0 +1,48 @@
+#! /usr/bin/env bash
+
+# - rofi (patched for Wayland)
+#   https://github.com/georglauterbach/hermes/releases/tag/rofi-v1.7.7%2Bwayland
+# - sway-audio-idle-inhibit (optional)
+#   https://github.com/ErikReider/SwayAudioIdleInhibit
+# - gpu-usage-waybar (optional)
+#   https://github.com/PolpOnline/gpu-usage-waybar
+
+set -eE -u -o pipefail
+shopt -s inherit_errexit
+
+if [[ ${EUID} -ne 0 ]]; then
+  echo "ERROR: This script needs to run with superuser privileges" >&2
+  exit 1
+fi
+
+source /etc/os-release || { echo "ERROR: Could not source '/etc/os-release'" ; exit 1 ; }
+
+case "${ID}" in
+  ( 'ubuntu' )
+    readonly PACKAGES=(
+      sway sway-backgrounds swayidle swaylock sway-notification-center xwayland
+      libvulkan1 mesa-vulkan-drivers vulkan-tools
+      xdg-desktop-portal xdg-desktop-portal-wlr xdg-desktop-portal-gtk
+      waybar
+      alacritty
+      evince feh grimshot nautilus
+      gnome-keyring
+      fonts-font-awesome
+      pipewire pipewire-audio-client-libraries pulseaudio-utils rtkit wireplumber
+      polkitd pkexec
+      at-spi2-core librsvg2-2
+    )
+
+    apt-get --yes install --no-install-recommends --no-install-suggests "${PACKAGES[@]}"
+
+    rm --force /etc/systemd/user/graphical-session.target.wants/waybar.service
+    apt-get --yes purge wmenu
+    systemctl --user disable waybar.service || :
+    systemctl --user enable --now pipewire-pulse.service
+    ;;
+
+  ( * )
+    echo "ERROR: Distribution '${ID}' is not supported"
+    exit 1
+    ;;
+esac
