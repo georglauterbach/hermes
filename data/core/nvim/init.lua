af9c821c8eae80a2c32933312e21b258d2531f25
Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>
2025-06-26 20:01:12 +0200

    feat: update NeoVIM configuration
    Signed-off-by: Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>

--- data/core/nvim/init.lua
+++ data/core/nvim/init.lua
@@ -1,101 +1,173 @@
 -- ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
 -- ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
 -- ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
--- ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║       version  2.0.0
--- ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║    sourced by  nvim
--- ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝          task  configure NeoVIM
+-- ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║       version  3.0.0-alpha.1
+-- ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║    sourced by  neovim
+-- ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝          task  configure neovim
 
--- Make sure to setup `mapleader` and `maplocalleader` before loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+--
+-- General
+--
 
-local configuration_options = {
-  backup = false,
-  writebackup = false,
-  undofile = true,
-  swapfile = false,
-  clipboard = "unnamedplus,unnamed",
-  number = true,
-  cursorline = true,
-  smartindent = true,
-  shiftwidth = 2,
-  tabstop = 2,
-  expandtab = true,
-  wrap = false,
-  scrolloff = 8,
-  sidescrolloff = 8,
-  termguicolors = true
-}
-for option_name, option_value in pairs(configuration_options) do vim.opt[option_name] = option_value end
+vim.scriptencoding = "utf-8"
 
--- the following configuration options only supports NeoVim version >= v0.10.0
+local global  = vim.g
+local options = vim.o
+local keymap  = vim.keymap
+
+--
+-- Options
+--
+
+global.loaded_perl_provider = 0
+global.loaded_ruby_provider = 0
+global.loaded_node_provider = 0
+
+global.show_whitespace      = 1
+
+options.number              = true
+options.cursorline          = true
+options.relativenumber      = true
+
+options.expandtab           = true  -- expand tab input with spaces characters
+options.smartindent         = true  -- syntax aware indentations for newline inserts
+options.tabstop             = 4     -- num of space characters per tab
+options.shiftwidth          = 4     -- spaces per indentation level
+
+--
+-- Key Bindings
+--
+
+global.mapleader            = " "
+global.maplocalleader       = " "
+
+keymap.set("n", "<leader>cd", vim.cmd.Ex)
+
+--
+-- Version-Specific Configuration
+--
+
 local version = vim.version()
 if version.major == 0 and version.minor < 10 then
   return
 end
 
--- Bootstrap lazy.nvim (https://github.com/folke/lazy.nvim)
--- https://lazy.folke.io/installation
+--
+-- Plugins (lazy.nvim - https://github.com/folke/lazy.nvim)
+--
+
+-- bootstrap (https://lazy.folke.io/installation)
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({{"Failed to clone lazy.nvim:\n", "ErrorMsg"}, {out, "WarningMsg"}, {"Press any key to exit..."}}, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+    local out = vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "--branch=stable",
+        "https://github.com/folke/lazy.nvim.git",
+        lazypath
+    })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            {"Failed to clone lazy.nvim:\n", "ErrorMsg"},
+            {out, "WarningMsg"},
+            {"Press any key to exit..."}},
+            true,
+            {}
+        )
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
-vim.opt.rtp:prepend(lazypath)
+vim.opt.rtp:append(lazypath)
 
--- Setup lazy.nvim
 require("lazy").setup({
-  spec = {
     {
-      "sainnhe/gruvbox-material",
-      lazy = false,
-      priority = 2000
+        "nvim-treesitter/nvim-treesitter",
+        tag = 'v0.9.3',
+        build = ":TSUpdate",
+        config = function ()
+            local configs = require("nvim-treesitter.configs")
+
+            configs.setup({
+                ensure_installed = {
+                    "bash",
+    	            "lua",
+    	            "markdown",
+    	            "markdown_inline",
+    	            "regex",
+    	            "vim"
+        	    },
+                sync_install = false,
+                highlight = { enable = true },
+                indent = { enable = true },
+            })
+        end
     },
     {
-      "sainnhe/everforest",
-      version = false,
-      lazy = false,
-      priority = 2000
+        "nvim-telescope/telescope.nvim",
+        tag = '0.1.8',
+	config = function()
+	    local builtin = require('telescope.builtin')
+	    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+	    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+	    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+	    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+	end
     },
     {
-      "f-person/auto-dark-mode.nvim",
-      lazy = false,
-      opts = {
-        update_interval = 2000,
-        set_dark_mode = function()
-          vim.api.nvim_set_option_value("background", "dark", {})
-          vim.cmd("colorscheme gruvbox-material")
-        end,
-        set_light_mode = function()
-          vim.api.nvim_set_option_value("background", "light", {})
-          vim.g.everforest_background = "hard"
-          vim.cmd("colorscheme everforest")
-        end,
-      },
+        "sainnhe/gruvbox-material",
+        lazy = false,
+        priority = 2000
     },
     {
-      "folke/noice.nvim",
-      lazy = true,
-      event = "VeryLazy",
-      opts = {
-        presets = {
-          bottom_search = false,        -- use a classic bottom cmdline for search
-          command_palette = true,       -- position the cmdline and popupmenu together
-          long_message_to_split = true, -- long messages will be sent to a split
-          inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = false,       -- add a border to hover docs and signature help
+        "sainnhe/everforest",
+        version = false,
+        lazy = false,
+        priority = 2000
+    },
+    {
+        "f-person/auto-dark-mode.nvim",
+        lazy = false,
+        opts = {
+            update_interval = 5000,
+            set_dark_mode = function()
+                vim.api.nvim_set_option_value("background", "dark", {})
+                vim.cmd("colorscheme gruvbox-material")
+            end,
+            set_light_mode = function()
+                vim.api.nvim_set_option_value("background", "light", {})
+                vim.g.everforest_background = "hard"
+                vim.cmd("colorscheme everforest")
+            end,
+        },
+    },
+    {
+        "folke/noice.nvim",
+        lazy = true,
+        event = "VeryLazy",
+        opts = {
+            presets = {
+              bottom_search = false,        -- use a classic bottom cmdline for search
+              command_palette = true,       -- position the cmdline and popupmenu together
+              long_message_to_split = true, -- long messages will be sent to a split
+              inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+              lsp_doc_border = false,       -- add a border to hover docs and signature help
+            },
+	        lsp = {
+		        override = {
+		            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+		            ["vim.lsp.util.stylize_markdown"] = true,
+		        }
+	        }
+        },
+        dependencies = {
+            {
+                "MunifTanjim/nui.nvim",
+                commit = "8d3bce9764e627b62b07424e0df77f680d47ffdb",
+            },
+            "rcarriga/nvim-notify"
         }
-      },
-      dependencies = {
-        "MunifTanjim/nui.nvim",
-        "rcarriga/nvim-notify"
-      }
     }
-  }
 })
+
