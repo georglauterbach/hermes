0cc63ce7d3f63e28a0b0f814bedb018d5a57c524
Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>
2025-10-15 20:55:29 +0200

    chore: prepare new release
    Signed-off-by: Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>

--- code/src/main.rs
+++ code/src/main.rs
@@ -3,12 +3,8 @@
 /// The `.tar.xz` archive created by `cupid`
 #[cfg(debug_assertions)]
 const ARCHIVE: &[u8] = &[];
-
-/// The `.tar.xz` archive created by `cupid`
 #[cfg(all(not(debug_assertions), target_arch = "x86_64"))]
 const ARCHIVE: &[u8] = include_bytes!("../../.assets/x86_64/archive.tar.xz");
-
-/// The `.tar.xz` archive created by `cupid`
 #[cfg(all(not(debug_assertions), target_arch = "aarch64"))]
 const ARCHIVE: &[u8] = include_bytes!("../../.assets/aarch64/archive.tar.xz");
 
