92317237fbcd14f95a25ce855eb1f044e54dd3f3
Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>
2026-01-26 09:46:44 +0100

    chore: update VS Code settings & extensions
    Signed-off-by: Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>

--- data/examples/vs_code/extensions.sh
+++ data/examples/vs_code/extensions.sh
@@ -4,24 +4,19 @@ set -eE -u -o pipefail
 shopt -s inherit_errexit
 
 readonly EXTENSIONS=(
-  'aaron-bond.better-comments'
   'asciidoctor.asciidoctor-vscode'
   'editorconfig.editorconfig'
-  'jonathanharty.gruvbox-material-icon-theme'
+  'edwinhuish.better-comments-next'
+  'georglauterbach.evergruv'
   'mads-hartmann.bash-ide-vscode'
   'ms-vscode-remote.remote-containers'
   'ms-vscode-remote.remote-ssh'
   'ms-vscode-remote.remote-ssh-edit'
   'ms-vscode.remote-explorer'
-  'sainnhe.everforest'
-  'sainnhe.gruvbox-material'
   'streetsidesoftware.code-spell-checker'
   'timonwong.shellcheck'
   'usernamehw.errorlens'
   'xshrim.txt-syntax'
-
-  # myriad-dreamin.tinymist
-  # mvllow.rose-pine
 )
 
 for EXTENSION in "${EXTENSIONS[@]}"; do
